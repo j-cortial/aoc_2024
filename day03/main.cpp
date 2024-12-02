6cbcf709aac6f21dac9ab33391fee1831d0a5deb
Julien Cortial <jcortial@gmail.com>
2024-12-02 22:51:38 +0100

    Prepare boilerplate for day 3

---
+++ day03/main.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <print>
+
+auto parse_input(std::istream&& in) {
+  return 0;
+}
+
+auto solve_part1(const auto& input) {
+    return 0;
+}
+
+auto solve_part2(const auto& input) {
+    return 0;
+}
+
+auto main() -> int {
+  const auto input = parse_input(std::ifstream{"input.txt"});
+  const auto answer1 = solve_part1(input);
+  std::println("The answer to part #1 is {}", answer1);
+  const auto answer2 = solve_part2(input);
+  std::println("The answer to part #1 is {}", answer2);
+}
