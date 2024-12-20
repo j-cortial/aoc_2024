6fc9edd0c78f43527c41e9522e363c125ffdcdaf
Julien Cortial <jcortial@gmail.com>
2024-12-21 00:07:33 +0100

    Solve day 14 part 1

--- day14/main.cpp
+++ day14/main.cpp
@@ -1,6 +1,10 @@
+#include <algorithm>
+#include <array>
 #include <cstdint>
 #include <fstream>
+#include <functional>
 #include <ios>
+#include <optional>
 #include <print>
 #include <ranges>
 #include <string>
@@ -42,8 +46,8 @@ auto parse_input(std::istream&& in) {
                              return std::stoi(token | std::ranges::to<std::string>());
                            });
              auto coords_it = std::ranges::begin(coords);
-             const auto row = *coords_it++;
              const auto col = *coords_it++;
+             const auto row = *coords_it++;
              return Loc{row, col};
            };
            const auto pos = parse(*section_it++);
@@ -53,7 +57,55 @@ auto parse_input(std::istream&& in) {
          std::ranges::to<std::vector>();
 }
 
-auto solve_part1(const auto& input) { return 0; }
+class Area {
+ public:
+  explicit Area(Loc lower_right) : lower_right_(lower_right) {}
+
+  auto tile(Loc target) const;
+
+  auto quadrant(Loc tile) const -> std::optional<std::uint_fast8_t>;
+
+ private:
+  Loc lower_right_;
+};
+
+auto Area::tile(Loc target) const {
+  const auto row = (target.row + lower_right_.row) % lower_right_.row;
+  const auto col = (target.col + lower_right_.col) % lower_right_.col;
+  return Loc{.row = row, .col = col};
+}
+
+auto Area::quadrant(Loc tile) const -> std::optional<std::uint_fast8_t> {
+  if (tile.row == (lower_right_.row / Idx{2}) || tile.col == (lower_right_.col / Idx{2})) {
+    return std::nullopt;
+  }
+
+  return {std::uint_fast8_t(tile.row > (lower_right_.row / Idx{2})) +
+          (std::uint_fast8_t(tile.col > (lower_right_.col / Idx{2})) * std::uint_fast8_t(2))};
+}
+
+auto solve_part1(const auto& input) {
+  const Area area{{.row = 103, .col = 101}};
+  auto robots = input;
+  for (auto i = 0; i != 100; ++i) {
+    for (auto& robot : robots) {
+      robot.pos = area.tile(robot.pos + robot.vel);
+    }
+  }
+
+  auto quadrants =
+      std::views::transform(robots,
+                            [&area](const Robot& robot) { return area.quadrant(robot.pos); }) |
+      std::views::filter([](const auto& maybe_quadrant) { return maybe_quadrant.has_value(); }) |
+      std::views::transform([](const auto& maybe_quadrant) { return *maybe_quadrant; });
+
+  std::array<std::size_t, 4> counts{};
+  for (const auto quadrant : quadrants) {
+    ++counts[quadrant];
+  }
+
+  return std::ranges::fold_left(counts, 1UZ, std::multiplies<>{});
+}
 
 auto solve_part2(const auto& input) { return 0; }
 
